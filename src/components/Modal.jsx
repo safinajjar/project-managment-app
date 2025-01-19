622d59a9321f68a679fb7603f765d4b5b5d20619
Musti <safi.najjar1@gmail.com>
2025-01-19 09:46:48 +0100

    error modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,33 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+import Button from "./Button";
+
+const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
+  const dialog = useRef(null);
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+      close() {
+        dialog.current.close();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog
+      ref={dialog}
+      className="backdrop:bg-stone-900/90 rounded-md shadow-sm w-[25rem] p-8"
+    >
+      {children}
+      <form method="dialog" className="mt-4 text-right">
+        <Button>{buttonCaption}</Button>
+      </form>
+    </dialog>,
+    document.getElementById("modal-root")
+  );
+});
+
+export default Modal;
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,15 +1,27 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
 export default function NewProject({ onCancelAddProject, onAddProject }) {
+  const modal = useRef(null);
   const projectName = useRef(null);
   const description = useRef(null);
   const dueDate = useRef(null);
 
   const handleSaveProject = () => {
-    const enteredName = projectName.current.value;
-    const enteredDescription = description.current.value;
-    const enteredDueDate = dueDate.current.value;
+    const enteredName = projectName.current.value.trim();
+    const enteredDescription = description.current.value.trim();
+    const enteredDueDate = dueDate.current.value.trim();
+
+    if (
+      enteredName === "" ||
+      enteredDescription === "" ||
+      enteredDueDate === ""
+    ) {
+      // show error modal
+      modal.current.open();
+      return;
+    }
 
     onAddProject({
       name: enteredName,
@@ -19,40 +31,47 @@ export default function NewProject({ onCancelAddProject, onAddProject }) {
   };
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button
-            onClick={onCancelAddProject}
-            className="text-stone-800 hover:text-stone-950"
-          >
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={handleSaveProject}
-            className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md"
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input
-          id="projectName"
-          type="text"
-          ref={projectName}
-          label="Project Name"
-        />
-        <Input
-          id="description"
-          ref={description}
-          label="Description"
-          textarea
-        />
-        <Input id="due_date" type="date" ref={dueDate} label="Due Date" />
+    <>
+      <Modal ref={modal} buttonCaption="OK">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">Please fill in all fields</p>
+      </Modal>
+
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button
+              onClick={onCancelAddProject}
+              className="text-stone-800 hover:text-stone-950"
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSaveProject}
+              className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input
+            id="projectName"
+            type="text"
+            ref={projectName}
+            label="Project Name"
+          />
+          <Input
+            id="description"
+            ref={description}
+            label="Description"
+            textarea
+          />
+          <Input id="due_date" type="date" ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
